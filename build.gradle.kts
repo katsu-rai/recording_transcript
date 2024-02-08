f561d26511bc5ac06ca689ba98d1dded622c2bbf
katsu-rai <taradancan0603@gmail.com>
2024-02-08 11:26:37 -0700

    the api worked

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("com.google.code.gson:gson:2.8.8")
 }
 
 tasks.test {
--- src/main/java/org/example/PostRequest.java
+++ src/main/java/org/example/PostRequest.java
@@ -1,75 +1,61 @@
 package org.example;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class PostRequest {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
 
-        HashMap<String, Object> carAttributes = new HashMap<>();
-        carAttributes.put("year", 2019);
-        carAttributes.put("price", 4999.99);
-        carAttributes.put("color", "blue");
-        Car car1 = new Car("BWM x5", carAttributes);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(car1);
+        Recording recording = new Recording();
+        recording.setAudio_url("https://github.com/katsu-rai/recording_transcript/raw/master/src/main/java/org/example/recording/thisisryan.mp3");
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(recording);
 
+        System.out.println(jsonRequest);
 
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", "669c94ad0ea94c51ba3b1d2322bb79b6")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                .build();
 
-        URL url = new URL("");
-        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-        conn.setRequestProperty("X-API-Key", "669c94ad0ea94c51ba3b1d2322bb79b6");
+        HttpClient httpClient = HttpClient.newHttpClient();
 
-        try (DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
-            dos.writeBytes(requestBody);
-        }
+        HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString());
 
-        try (BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-            String line;
-            while ((line = bf.readLine()) != null) {
-                System.out.println(line);
+        System.out.println(postResponse.body());
+
+        recording = gson.fromJson(postResponse.body(), Recording.class);
+
+        System.out.println(recording.getStatus());
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript/" + recording.getId()))
+                .header("Authorization", "669c94ad0ea94c51ba3b1d2322bb79b6")
+                .build();
+
+        while (true){
+            HttpResponse<String> getResponse = httpClient.send(getRequest, BodyHandlers.ofString());
+            recording = gson.fromJson(getResponse.body(), Recording.class);
+
+            System.out.println(recording.getStatus());
+
+            if("completed".equals(recording.getStatus()) || "error".equals(recording.getStatus())){
+                break;
             }
+            Thread.sleep(1000);
         }
 
+        System.out.println("Transcript completed!");
+        System.out.println(recording.getText());
 
-//        HashMap<String, Object> carAttributes = new HashMap<>();
-//        carAttributes.put("year", 2019);
-//        carAttributes.put("price", 4999.99);
-//        carAttributes.put("color", "blue");
-//        Car car1 = new Car("BWM x5", carAttributes);
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        String requestBody = objectMapper.writeValueAsString(car1);
-//
-//
-//
-//        URL url = new URL("https://api.restful-api.dev/objects");
-//        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-//        conn.setRequestMethod("POST");
-//        conn.setDoOutput(true);
-//        conn.setRequestProperty("Content-Type", "application/json");
-//        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-//        conn.setRequestProperty("X-API-Key", "669c94ad0ea94c51ba3b1d2322bb79b6");
-//
-//        try (DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
-//            dos.writeBytes(requestBody);
-//        }
-//
-//        try (BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-//            String line;
-//            while ((line = bf.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//        }
+
     }
 }
